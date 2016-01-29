f9a370188a3ba15a4abf04af2b3b6c58801f7b5e
Jason Hill <michaelcmcghee@gmail.com>
2016-01-29 16:56:09 -0500

    Adding the files

---
+++ ext/src/bg/background.js
@@ -0,0 +1,7 @@
+document.body.innerHTML = document.body.innerHTML.replace(new RegExp("never", "g"), "always");
+document.body.innerHTML = document.body.innerHTML.replace(new RegExp("Never", "g"), "Always");
+document.body.innerHTML = document.body.innerHTML.replace(new RegExp("shouldn't", "g"), "should");
+document.body.innerHTML = document.body.innerHTML.replace(new RegExp("Shouldn't", "g"), "Should");
+document.body.innerHTML = document.body.innerHTML.replace(new RegExp("don't", "g"), "do");
+document.body.innerHTML = document.body.innerHTML.replace(new RegExp("Don't", "g"), "Do");
+
