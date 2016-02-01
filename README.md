c13adc82510d85d539a627058c537fdc68a638ec
Jason Hill <michaelcmcghee@gmail.com>
2016-02-01 09:50:44 -0500

    Added a few more words to the list

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # Bad-Advice
-Chrome Extension
 
 This Chrome extension replaces all instances of never with always, shouldn't with should and don't with do.  It's generally a bad idea to install this and I am not responsible for any damages, and or fun that may occur becuase of it.  You have been warned.
--- ext/src/bg/background.js
+++ ext/src/bg/background.js
@@ -1,7 +1,14 @@
+//originals
 document.body.innerHTML = document.body.innerHTML.replace(new RegExp("never", "g"), "always");
 document.body.innerHTML = document.body.innerHTML.replace(new RegExp("Never", "g"), "Always");
 document.body.innerHTML = document.body.innerHTML.replace(new RegExp("shouldn't", "g"), "should");
 document.body.innerHTML = document.body.innerHTML.replace(new RegExp("Shouldn't", "g"), "Should");
 document.body.innerHTML = document.body.innerHTML.replace(new RegExp("don't", "g"), "do");
 document.body.innerHTML = document.body.innerHTML.replace(new RegExp("Don't", "g"), "Do");
-
+document.body.innerHTML = document.body.innerHTML.replace(new RegExp("bad", "g"), "good");
+document.body.innerHTML = document.body.innerHTML.replace(new RegExp("Bad", "g"), "Good");
+//secondary
+document.body.innerHTML = document.body.innerHTML.replace(new RegExp("aren't", "g"), "are");
+document.body.innerHTML = document.body.innerHTML.replace(new RegExp("Aren't", "g"), "Are");
+document.body.innerHTML = document.body.innerHTML.replace(new RegExp("wouldn't", "g"), "would");
+document.body.innerHTML = document.body.innerHTML.replace(new RegExp("Wouldn't", "g"), "Would");
